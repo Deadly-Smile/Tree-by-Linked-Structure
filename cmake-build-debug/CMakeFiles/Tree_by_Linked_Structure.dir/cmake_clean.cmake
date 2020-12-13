212691a8dd3b660b47084f82c838e27578d0d526
Anik Saha <aniksaha200r@gmail.com>
2020-12-13 22:40:47 +0600

    Other files

---
+++ cmake-build-debug/CMakeFiles/Tree_by_Linked_Structure.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tree_by_Linked_Structure.dir/main.cpp.obj"
+  "Tree_by_Linked_Structure.exe"
+  "Tree_by_Linked_Structure.exe.manifest"
+  "Tree_by_Linked_Structure.pdb"
+  "libTree_by_Linked_Structure.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tree_by_Linked_Structure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Tree_by_Linked_Structure.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Tree_by_Linked_Structure.
+# This may be replaced when dependencies are built.
